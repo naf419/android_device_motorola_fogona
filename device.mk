d7665fc416a6b2740fbded93fa04cbb3c738a895
naf <naf@sdf.org>
2026-08-15 07:56:21 -0500

    additional changes needed from 21->22.2

--- device.mk
+++ device.mk
@@ -314,7 +314,7 @@ PRODUCT_BOOT_JARS += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.qti-v2
+    android.hardware.thermal-service.qti
 
 # Vendor service manager
 PRODUCT_PACKAGES += \
