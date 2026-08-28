e01ff7f87c9e2c486e9a4868e8d2a4f4badfa606
naf <naf@sdf.org>
2026-08-28 10:09:30 -0500

    move zsmalloc/zram kmods from system_dlkm to vendor_dlkm
    because lineage-23.2 kernel build doesn't like that vendor_dlkm kmods
    have dependencies in system_dlkm, so build errors out like so:
    moto_swap.ko needs unknown symbol zs_malloc
    ERROR: kernel module(s) need unknown symbol(s)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -69,14 +69,12 @@ TARGET_KERNEL_EXT_MODULE_ROOT := kernel/motorola/sm8550-modules
 BOARD_USES_GENERIC_KERNEL_IMAGE := true
 TARGET_HAS_GENERIC_KERNEL_HEADERS := true
 
-BOARD_SYSTEM_KERNEL_MODULES_LOAD := $(strip $(shell cat $(LOCAL_PATH)/modules.load.system_dlkm))
 BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(LOCAL_PATH)/modules.load))
 BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE := $(LOCAL_PATH)/modules.blocklist
 BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD := $(strip $(shell cat $(LOCAL_PATH)/modules.load.vendor_boot))
 BOARD_VENDOR_RAMDISK_KERNEL_MODULES_BLOCKLIST_FILE := $(LOCAL_PATH)/modules.blocklist
 BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(strip $(shell cat $(LOCAL_PATH)/modules.load.recovery))
 BOOT_KERNEL_MODULES := $(strip $(shell cat $(LOCAL_PATH)/modules.load $(LOCAL_PATH)/modules.load.vendor_boot))
-SYSTEM_KERNEL_MODULES := $(strip $(shell cat $(LOCAL_PATH)/modules.load.system_dlkm))
 
 TARGET_KERNEL_EXT_MODULES := \
     qcom/opensource/mmrm-driver \
